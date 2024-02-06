c8ab60a55e26ade24835cf7e746c289048866384
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2024-02-06 17:55:10 +0900

    .

---
+++ Web/HanulFinalB/src/main/java/com/hanul/finalb/qna/copy/QnaController.java
@@ -0,0 +1,12 @@
+package com.hanul.finalb.qna.copy;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class QnaController {
+	
+	
+	
+	public void 
+
+}
---
+++ Web/HanulFinalB/src/main/java/com/hanul/qna/QnaController.java
@@ -0,0 +1,12 @@
+package com.hanul.qna;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class QnaController {
+	
+	
+	
+	public void 
+
+}
