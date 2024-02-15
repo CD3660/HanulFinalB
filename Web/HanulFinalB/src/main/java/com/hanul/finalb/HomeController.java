189f3f18ef518d8ac16722517b80452b8d7423e4
박찬영 <144075327+CD3660@users.noreply.github.com>
2024-02-15 18:08:27 +0900

    aa

--- Web/HanulFinalB/src/main/java/com/hanul/finalb/HomeController.java
+++ Web/HanulFinalB/src/main/java/com/hanul/finalb/HomeController.java
@@ -1,27 +1,26 @@
 package com.hanul.finalb;
 
-import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.hanul.finalb.product.ProductVO;
-
 
 @Controller
 public class HomeController {
-	@Autowired private HomeService service;
-	
-	
+
+	@Autowired
+	private HomeService service;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
 
@@ -30,14 +29,17 @@ public class HomeController {
 		list.add(service.product_info(3));
 		list.add(service.product_info(4));
 		list.add(service.product_info(6));
-	
+
 		model.addAttribute("list", list);
-		
+
 		return "home/home";
 	}
-	
-	
-	
-	
-	
+
+	@ResponseBody
+	@RequestMapping(value = "/sensor/data_in")
+	public String sensor(int dust, int gas, int fire, int temp, int humid) {
+		
+		return "connect success";
+	}
+
 }
