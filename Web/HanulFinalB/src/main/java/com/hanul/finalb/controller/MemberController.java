9e299e7b13f0d0425d29aee9e4e50cd59c9608fd
Chunghyeon Bae <144658490+ChungHyeonBae@users.noreply.github.com>
2024-02-16 17:40:26 +0900

    1
    1

--- Web/HanulFinalB/src/main/java/com/hanul/finalb/controller/MemberController.java
+++ Web/HanulFinalB/src/main/java/com/hanul/finalb/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.hanul.finalb.controller;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,9 @@ import com.hanul.finalb.member.MemberVO;
 @Controller
 @RequestMapping("/member")
 public class MemberController {
+	
+	@Autowired
+	BCryptPasswordEncoder pwEncoder;
 
 	@Autowired
 	private MemberService service;
@@ -33,17 +37,50 @@ public class MemberController {
 	}
 
 	@RequestMapping(path = "/join", method = RequestMethod.POST)
-	public String finalb(MemberVO member) throws Exception {
+	public String joinpass(MemberVO member) throws Exception {
 		
 		service.memberJoin(member);
 
 		return "redirect:/member/login";
+		
 
 	
 	
 
 	}
+	@RequestMapping(path = "/login", method = RequestMethod.POST)
+	public String loginpass(MemberVO member) throws Exception {
+		
+		service.memberJoin(member);
+
+		return "redirect:/member/login";
+		
+
 	
+	
+
+	}
+	@RequestMapping(value = "/secuTest", method = RequestMethod.GET)
+    public void secuTest() {
+        
+        String rawPassword = "hanul123";                //인코딩 전 메서드
+        String encdoePassword1;                        // 인코딩된 메서드
+        String encdoePassword2;                        // 똑같은 비밀번호 데이터를 encdoe()메서드를 사용했을 때 동일한 인코딩된 값이 나오는지 확인하기 위해 추가
+        
+        encdoePassword1 = pwEncoder.encode(rawPassword);
+        encdoePassword2 = pwEncoder.encode(rawPassword);
+        
+        // 인코딩된 패스워드 출력
+        System.out.println("encdoePassword1 : " +encdoePassword1);
+        System.out.println(" encdoePassword2 : " + encdoePassword2);
+        
+        String truePassowrd = "hanul123";
+        String falsePassword = "asdfjlasf";
+        
+        System.out.println("truePassword verify : " + pwEncoder.matches(truePassowrd, encdoePassword1));
+        System.out.println("falsePassword verify : " + pwEncoder.matches(falsePassword, encdoePassword1));    
+    
+    }
 	
 	
 	
