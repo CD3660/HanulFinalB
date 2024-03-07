09dc3c747b6a7b1733e1fa492656e71b4890302f
박찬영 <144075327+CD3660@users.noreply.github.com>
2024-03-07 17:09:50 +0900

    a

--- Web/HanulFinalB/src/main/java/com/hanul/finalb/shop/OrderVO.java
+++ Web/HanulFinalB/src/main/java/com/hanul/finalb/shop/OrderVO.java
@@ -13,5 +13,6 @@ public class OrderVO {
 	private int id, prod_id, ea, price;
 	private Date order_date;
 	private String address, phone, reciever, request_msg, pay_now, imp_uid, merchant_uid, user_id;
+	
 	private ProductVO prod;
 }
