086932f045266f06ae476ae78e692e146d5fffea
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2024-03-22 13:03:46 +0900

    .

--- Web/HanulFinalB/src/main/java/com/hanul/finalb/controller/AppController.java
+++ Web/HanulFinalB/src/main/java/com/hanul/finalb/controller/AppController.java
@@ -170,10 +170,10 @@ public class AppController {
 	
 	
 	@RequestMapping("/qnaComment")
-	public String qnaCommentList() {
+	public String qnaCommentList(int qna_id) {
 		
 		//서비스에서 List로 보내고, 그걸 받아서 ArrayList로 변환해줌
-		ArrayList<QnaCommentVO> list = (ArrayList<QnaCommentVO>)qnaService.appQnaCommentList();
+		ArrayList<QnaCommentVO> list = (ArrayList<QnaCommentVO>)qnaService.appQnaCommentList(qna_id);
 
 	    // 생성된 Type을 사용하여 리스트를 JSON 문자열로 변환
 		System.out.println(">> "+ new Gson().toJson(list));
--- Web/HanulFinalB/src/main/java/com/hanul/finalb/qna/QnaService.java
+++ Web/HanulFinalB/src/main/java/com/hanul/finalb/qna/QnaService.java
@@ -30,9 +30,9 @@ public class QnaService {
 	
 	
 	//안드로이드 코멘트 목록 조회
-	public List<QnaCommentVO> appQnaCommentList() {
+	public List<QnaCommentVO> appQnaCommentList(int qna_id) {
 		
-		List<QnaCommentVO> list = sql.selectList("qna.appQnaCommentList");
+		List<QnaCommentVO> list = sql.selectList("qna.appQnaCommentList", qna_id);
 		return list;
 	}
 	
